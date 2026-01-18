9029364d4a7b0438e80fec298f83fac3419768e1
eltoncornelson123-ui <eltoncornelson123@gmail.com>
2026-01-18 17:51:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("bookingForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for booking with Amapretty Records! We’ll contact you soon.");
+});
